392c4f9880b1ee86097ee8cde25d5ccefb0ca8d3
heejin <heejin3602@naver.com>
2023-01-31 14:14:25 +0900

    message

--- src/main/java/com/mpc/board/controller/BoardController.java
+++ src/main/java/com/mpc/board/controller/BoardController.java
@@ -243,3 +243,4 @@ public class BoardController {
 		}
 		return "board/search";
 }
+}
--- src/main/java/com/mpc/consult/service/UserService.java
+++ src/main/java/com/mpc/consult/service/UserService.java
@@ -2,39 +2,94 @@ package com.mpc.consult.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.mpc.user.dao.IUserRepository;
+import com.mpc.user.dao.UserRepository;
 import com.mpc.user.model.UserModel;
 
-public class UserService implements IUserRepository {
+public class UserService implements UserRepository {
 
-@Autowired IUserRepository userRepository;
-
-	@Override
-	public void signup(UserModel info) {
-		userRepository.signup();
-	}
+@Autowired UserRepository userRepository;
 
 	@Override
-	public int idChk() {
-
+	public int idChk(UserModel model) {
+		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public int nickChk() {
+	public void petUpdate() {
+		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void petAdd() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void likeList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void scrapList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void followList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public UserModel login(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void logout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void myPageSearch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void myPageUpdate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void signup(UserModel user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int nickChk() {
+		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int emailChk() {
-
+		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int pwValChk() {
-
+		// TODO Auto-generated method stub
 		return 0;
 	}
 
